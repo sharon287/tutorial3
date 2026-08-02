67d4597a374c489e261011763c40fc533916c186
sharon287 <sharonabraham2029@cs.sjcetpalai.ac.in>
2026-08-02 21:28:06 +0530

    Create ArrayDisplay.java

---
+++ ArrayDisplay.java
@@ -0,0 +1,35 @@
+package mod1;
+import java.util.Scanner;
+
+public class ArrayDisplay {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements: ");
+        int n = sc.nextInt();
+
+        int[] array = new int[n];
+
+        System.out.println("Enter the elements:");
+
+        for (int i = 0; i < n; i++) {
+            array[i] = sc.nextInt();
+        }
+
+        System.out.println("Array in same order:");
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(array[i] + " ");
+        }
+
+        System.out.println("\nArray in reverse order:");
+
+        for (int i = n - 1; i >= 0; i--) {
+            System.out.print(array[i] + " ");
+        }
+
+        sc.close();
+    }
+}
